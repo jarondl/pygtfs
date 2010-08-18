fc8b2eb02d1c0c803c56504c1f93a9c8a3af1042
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-17 20:53:57 -0700

    agency, route, and trip tables partially mapped

--- transitfeed/main.py
+++ transitfeed/main.py
@@ -5,7 +5,15 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 import os
 from codecs import iterdecode
 from zipfile import ZipFile
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+
+class GTFSForeignKey(str):
+  pass
+
+def make_gtfs_foreign_key_class(cls):
+  class ret(GTFSForeignKey):
+    _cls = cls
+  return ret
 
 class GTFSField(object):
   def __init__(self, fieldtype):
@@ -28,26 +36,20 @@ class GTFSEntity(object):
       if attrname in kwargs:
         setattr( self, attrname, attrtype( kwargs[attrname] ) )
 
-metadata = MetaData()
-agency_table = Table('agency', metadata,
-    Column('agency_id', String, primary_key=True),
-    Column('agency_name', String),
-    Column('agency_url', String),
-    Column('agency_timezone', String),
-    Column('agency_lang', String),
-    Column('agency_phone', String)
-)
 
 def table_def_from_entity(entity_class, metadata):
   sqlalchemy_types = {str:String,int:Integer}
   columns = []
   for field_name,field_type in entity_class.FIELDS:
-    columns.append( Column( field_name,
-                            sqlite_types[field_type],
-			    primary_key=(field_name==entity_class.ID_FIELD) ) )
-
+    if issubclass(field_type, GTFSForeignKey):
+      foreign_key_column_name = field_type._cls.TABLENAME+"."+field_type._cls.ID_FIELD
+      columns.append( Column( field_name, String, ForeignKey(foreign_key_column_name) ) )
+    else:
+      columns.append( Column( field_name,
+                              sqlalchemy_types[field_type],
+                              primary_key=(field_name==entity_class.ID_FIELD) ) )
+  
   return Table( entity_class.TABLENAME, metadata, *columns )
-    
 
 class Agency(GTFSEntity):
   TABLENAME = "agency"
@@ -61,6 +63,45 @@ class Agency(GTFSEntity):
 
   def __init__(self, **kwargs):
     GTFSEntity.__init__(self, **kwargs)
+
+  def __repr__(self):
+    return "<Agency %s>"%self.agency_id
+
+class Route(GTFSEntity):
+  TABLENAME = "routes"
+  FIELDS = (('route_id',str),
+            ('agency_id',make_gtfs_foreign_key_class(Agency)),
+	    ('route_short_name',str),
+	    ('route_long_name',str),
+	    ('route_desc',str),
+	    ('route_type',int),
+	    ('route_url',str),
+	    ('route_color',str),
+	    ('route_text_color',str))
+  ID_FIELD = "route_id"
+
+  def __repr__(self):
+    return "<Route %s>"%self.route_id
+
+class Trip(GTFSEntity):
+  TABLENAME = "trips"
+  FIELDS = (('route_id',make_gtfs_foreign_key_class(Route)),
+            ('service_id',str),
+	    ('trip_id',str),
+	    ('trip_headsign',str),
+	    ('trip_short_name',str),
+	    ('direction_id',str),
+	    ('block_id',str),
+	    ('shape_id',str))
+  ID_FIELD = "trip_id"
+
+  def __repr__(self):
+    return "<Trip %s>"%self.trip_id
+
+metadata = MetaData()
+agency_table = table_def_from_entity( Agency, metadata )
+routes_table = table_def_from_entity( Route, metadata )
+trips_table = table_def_from_entity( Trip, metadata )
 
 class Record(object):
   def __init__(self,header,row):
@@ -114,12 +155,7 @@ class Feed(object):
     rows = self.get_rows( filename )
     return Table( rows.next(), rows )
 
-if __name__=='__main__':
-  engine = create_engine('sqlite:////home/brandon/Desktop/test.db', echo=True)
-  metadata.create_all(engine) 
-  mapper(Agency, agency_table) 
-  Session = sessionmaker(bind=engine)
-  session = Session()
+def load(session):
 
   feed = Feed( "/home/brandon/Desktop/bart.zip" )
   agency_table = feed.get_table( "agency.txt" )
@@ -128,4 +164,31 @@ if __name__=='__main__':
     agency = Agency( **record.to_dict() )
     session.add( agency )
 
+  for record in feed.get_table( "routes.txt" ):
+    route = Route( **record.to_dict() )
+    session.add( route )
+
+  for record in feed.get_table( "trips.txt" ):
+    trip = Trip( **record.to_dict() )
+    session.add( trip )
+
   session.commit()
+
+def query(session):
+  for agency in session.query(Agency).filter(Agency.agency_id=="BART"):
+    print agency.routes[0].trips
+
+  #for route in session.query(Route):
+  #  print route
+
+if __name__=='__main__':
+  engine = create_engine('sqlite:////home/brandon/Desktop/test.db', echo=True)
+  metadata.create_all(engine) 
+  mapper(Agency, agency_table, properties={'routes':relationship(Route)}) 
+  mapper(Route, routes_table, properties={'agency':relationship(Agency),'trips':relationship(Trip)})
+  mapper(Trip, trips_table, properties={'route':relationship(Route)})
+  Session = sessionmaker(bind=engine)
+  session = Session()
+
+  #load(session)
+  query(session)
