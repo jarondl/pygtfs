cfe47c02d334a9bb68cf6946d2a836e1683a14f0
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-19 14:58:27 -0700

    a bit of cleanup

--- transitfeed/main.py
+++ transitfeed/main.py
@@ -1,58 +1,73 @@
 import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import mapper
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.sql.expression import ColumnElement
+from sqlalchemy.orm import relationship
 
 from entity import *
 from gtfs import GTFSForeignKey
 import feed
 
 def table_def_from_entity(entity_class, metadata):
-  sqlalchemy_types = {str:String,int:Integer}
+  sqlalchemy_types = {str:sqlalchemy.String,int:sqlalchemy.Integer}
   columns = []
   for field_name,field_type in entity_class.FIELDS:
     if issubclass(field_type, GTFSForeignKey):
       foreign_key_column_name = field_type._cls.TABLENAME+"."+field_type._cls.ID_FIELD
-      columns.append( Column( field_name, String, ForeignKey(foreign_key_column_name) ) )
+      columns.append( sqlalchemy.Column( field_name, sqlalchemy.String, sqlalchemy.ForeignKey(foreign_key_column_name) ) )
     else:
-      columns.append( Column( field_name,
+      columns.append( sqlalchemy.Column( field_name,
                               sqlalchemy_types[field_type],
                               primary_key=(field_name==entity_class.ID_FIELD) ) )
   if entity_class.ID_FIELD is None:
-    columns.append( Column( "id", Integer, primary_key=True ) )
+    columns.append( sqlalchemy.Column( "id", sqlalchemy.Integer, primary_key=True ) )
   
-  return Table( entity_class.TABLENAME, metadata, *columns )
+  return sqlalchemy.Table( entity_class.TABLENAME, metadata, *columns )
 
-metadata = MetaData()
-agency_table = table_def_from_entity( Agency, metadata )
-routes_table = table_def_from_entity( Route, metadata )
-trips_table = table_def_from_entity( Trip, metadata )
-stop_times_table = table_def_from_entity( StopTime, metadata )
-calendar_table = table_def_from_entity( ServicePeriod, metadata )
-calendar_dates_table = table_def_from_entity( ServiceException, metadata )
-fare_attributes_table = table_def_from_entity( Fare, metadata )
-fare_rules_table = table_def_from_entity( FareRule, metadata )
-stops_table = table_def_from_entity( Stop, metadata )
-shapes_table = table_def_from_entity( ShapePoint, metadata )
-frequencies_table = table_def_from_entity( Frequency, metadata )
-transfers_table = table_def_from_entity( Transfer, metadata )
+def create_and_map_tables(metadata):
+  # create the tables
+  agency_table = table_def_from_entity( Agency, metadata )
+  routes_table = table_def_from_entity( Route, metadata )
+  trips_table = table_def_from_entity( Trip, metadata )
+  stop_times_table = table_def_from_entity( StopTime, metadata )
+  calendar_table = table_def_from_entity( ServicePeriod, metadata )
+  calendar_dates_table = table_def_from_entity( ServiceException, metadata )
+  fare_attributes_table = table_def_from_entity( Fare, metadata )
+  fare_rules_table = table_def_from_entity( FareRule, metadata )
+  stops_table = table_def_from_entity( Stop, metadata )
+  shapes_table = table_def_from_entity( ShapePoint, metadata )
+  frequencies_table = table_def_from_entity( Frequency, metadata )
+  transfers_table = table_def_from_entity( Transfer, metadata )
+
+  # map the tables
+  sqlalchemy.orm.mapper(Agency, agency_table, properties={'routes':relationship(Route)}) 
+  sqlalchemy.orm.mapper(Route, routes_table, properties={'agency':relationship(Agency),'trips':relationship(Trip),'fare_rules':relationship(FareRule)})
+  sqlalchemy.orm.mapper(Stop, stops_table)
+  sqlalchemy.orm.mapper(Trip, trips_table, properties={'route':relationship(Route),
+                                        'stop_times':relationship(StopTime),
+					'service_period':relationship(ServicePeriod),
+					'frequencies':relationship(Frequency)})
+  sqlalchemy.orm.mapper(StopTime, stop_times_table, properties={'trip':relationship(Trip),'stop':relationship(Stop)})
+  sqlalchemy.orm.mapper(ServicePeriod, calendar_table,properties={'trips':relationship(Trip),'exceptions':relationship(ServiceException)})
+  sqlalchemy.orm.mapper(ServiceException, calendar_dates_table, properties={'calendar':relationship(ServicePeriod)})
+  sqlalchemy.orm.mapper(Fare, fare_attributes_table, properties={'rules':relationship(FareRule)})
+  sqlalchemy.orm.mapper(FareRule, fare_rules_table, properties={'fare':relationship(Fare),'route':relationship(Route)})
+  sqlalchemy.orm.mapper(ShapePoint, shapes_table)
+  sqlalchemy.orm.mapper(Frequency, frequencies_table, properties={'trip':relationship(Trip)})
+  sqlalchemy.orm.mapper(Transfer, transfers_table, properties={"from_stop":relationship(Stop,primaryjoin=transfers_table.c.from_stop_id==stops_table.c.stop_id),
+                                                "to_stop":relationship(Stop,primaryjoin=transfers_table.c.to_stop_id==stops_table.c.stop_id)})
 
 def load(session):
 
   fd = feed.Feed( "/home/brandon/Desktop/bart.zip" )
 
   for gtfs_class in (Agency, 
-                     #Route, 
+                     Route, 
 		     Stop,
 		     Trip, 
-		     #StopTime,
-		     #ServicePeriod, 
-		     #ServiceException, 
-		     #Fare,
-		     #FareRule,
-		     #ShapePoint,
+		     StopTime,
+		     ServicePeriod, 
+		     ServiceException, 
+		     Fare,
+		     FareRule,
+		     ShapePoint,
 		     Frequency,
 		     Transfer,
 		     ):
@@ -103,26 +118,14 @@ def query(session):
 
 
 if __name__=='__main__':
-  engine = create_engine('sqlite:////home/brandon/Desktop/test.db', echo=False)
-  metadata.create_all(engine) 
-  mapper(Agency, agency_table, properties={'routes':relationship(Route)}) 
-  mapper(Route, routes_table, properties={'agency':relationship(Agency),'trips':relationship(Trip),'fare_rules':relationship(FareRule)})
-  mapper(Stop, stops_table)
-  mapper(Trip, trips_table, properties={'route':relationship(Route),
-                                        'stop_times':relationship(StopTime),
-					'service_period':relationship(ServicePeriod),
-					'frequencies':relationship(Frequency)})
-  mapper(StopTime, stop_times_table, properties={'trip':relationship(Trip),'stop':relationship(Stop)})
-  mapper(ServicePeriod, calendar_table,properties={'trips':relationship(Trip),'exceptions':relationship(ServiceException)})
-  mapper(ServiceException, calendar_dates_table, properties={'calendar':relationship(ServicePeriod)})
-  mapper(Fare, fare_attributes_table, properties={'rules':relationship(FareRule)})
-  mapper(FareRule, fare_rules_table, properties={'fare':relationship(Fare),'route':relationship(Route)})
-  mapper(ShapePoint, shapes_table)
-  mapper(Frequency, frequencies_table, properties={'trip':relationship(Trip)})
-  mapper(Transfer, transfers_table, properties={"from_stop":relationship(Stop,primaryjoin=transfers_table.c.from_stop_id==stops_table.c.stop_id),
-                                                "to_stop":relationship(Stop,primaryjoin=transfers_table.c.to_stop_id==stops_table.c.stop_id)})
+  metadata = sqlalchemy.MetaData()
 
-  Session = sessionmaker(bind=engine)
+  create_and_map_tables(metadata)
+
+  engine = sqlalchemy.create_engine('sqlite:////home/brandon/Desktop/test.db', echo=False)
+  metadata.create_all(engine) 
+
+  Session = sqlalchemy.orm.sessionmaker(bind=engine)
   session = Session()
 
   load(session)
