323fc59c8c345042df1a37ae35b7ad8cc7660efa
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-20 14:57:50 -0700

    install script

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ setup(
     name = "gtfs",
     version = "0.1",
     packages = find_packages(),
+    entry_points = {
+      "console_scripts": ["compile_gtfs = gtfs.script.compile_gtfs:main"]
+    }
 )
