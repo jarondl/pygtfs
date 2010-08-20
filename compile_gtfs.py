c275e0f70744840b400b2e377df0a47e861535b0
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-19 21:21:05 -0700

    script to compile GTFS files into sqlite files

---
+++ compile_gtfs.py
@@ -0,0 +1,28 @@
+from gtfsparser.loader import load
+from gtfsparser import metadata
+from gtfsparser.schedule import Schedule
+
+from optparse import OptionParser
+import os
+
+def main():
+  usage = "usage: %prog [options] gtfs_filename"
+  parser = OptionParser(usage)
+  parser.add_option( "-o", "--output_filename", dest="output_filename" )
+
+  options, args = parser.parse_args()
+
+  if len(args) != 1:
+    parser.error( "No gtfs filename supplied" )
+
+  gtfs_filename = args[0]
+
+  if options.output_filename:
+    output_filename = options.output_filename
+  else:
+    output_filename = os.path.splitext( gtfs_filename )[0]+".db"
+
+  load( metadata, gtfs_filename, output_filename )
+
+if __name__=='__main__':
+  main()
