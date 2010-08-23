d0d34689591c52f92108d553442527a978a09b3d
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-22 19:55:26 -0700

    increment version before pypi push

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "gtfs",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
     install_requires=['sqlalchemy>=0.6.3'],
     entry_points = {
