23dc86fe7aba48b8600fc0849a440ff25427df1f
Brandon Martin-Anderson <badhill@gmail.com>
2010-08-22 19:54:24 -0700

    require sqlalchemy

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name = "gtfs",
     version = "0.1",
     packages = find_packages(),
+    install_requires=['sqlalchemy>=0.6.3'],
     entry_points = {
       "console_scripts": ["compile_gtfs = gtfs.scripts.compile_gtfs:main"]
     }
