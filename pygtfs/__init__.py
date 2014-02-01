ab82ba115441ce60fb04cefc06dfa2a83ea772fc
Spencer Gardner <spencergardner@gmail.com>
2014-02-01 15:37:02 -0600

    add module for exporting the data back out to csv, partially complete

--- pygtfs/__init__.py
+++ pygtfs/__init__.py
@@ -1,4 +1,5 @@
 from .loader import append_feed, delete_feed, overwrite_feed, list_feeds
 from .schedule import Schedule
+from .exporter import export_feed, export_shapefile
 
-__version__ = "0.1.2"
+__version__ = "0.1.2.1"
