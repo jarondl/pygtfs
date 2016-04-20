c54240e6d9f6393370fe94f2cd05476680cf17f2
Yaron de Leeuw <me@jarondl.net>
2016-04-20 20:06:31 +0300

    Allow usage directly from the code (fix the _version import)

--- pygtfs/__init__.py
+++ pygtfs/__init__.py
@@ -1,3 +1,10 @@
+import warnings
+
 from .loader import append_feed, delete_feed, overwrite_feed, list_feeds
 from .schedule import Schedule
-from ._version import version as __version__
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    warnings.warn("pygtfs should be installed for the version to work")
+    __version__ = "0"
